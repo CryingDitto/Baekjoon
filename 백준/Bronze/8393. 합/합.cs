0b41979dff05f4135a14ed7454aa81010d183f0d
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-09-20 17:01:06 +0900

    [Bronze V] Title: 합, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cs
+++ 백준/Bronze/8393. 합/합.cs
@@ -1,7 +1,13 @@
-int sum = 0;
-int input = int.Parse(Console.ReadLine());
-for (int i = 1; i < input+1; i++)
-{
-    sum += i;
-}
-Console.WriteLine(sum);
+//int sum = 0;
+//int input = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i < input+1; i++)
+//{
+//    sum += i;
+//}
+//Console.WriteLine(sum);
+
+// 다른 방법
+// 1부터 n까지의 합 공식 n * (n+1) / 2 
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(n * (n + 1) / 2);
