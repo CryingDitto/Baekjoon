83dd91eb92a2925e9d8f6f47d2e9f4b55816d6b9
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-09-26 20:50:26 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -3,22 +3,27 @@ public class Solution {
         long answer = 0;
         long la = (long)a;
         long lb = (long)b;
+        answer = la;
+        while(la!=lb){
+            la = (la-lb)>0 ? la-1 : la+1;    
+            answer += la;        
+        }
         
-        if (la-lb > 0){
-            while (la >= lb){
-                answer += la;
-                la--;
-            }
-        }
-        else if(la-lb < 0){
-            while (la <= lb){
-                answer += la;
-                la++;
-            }
-        }
-        else{
-            answer = la;
-        }
+        // if (la-lb > 0){
+        //     while (la >= lb){
+        //         answer += la;
+        //         la--;
+        //     }
+        // }
+        // else if(la-lb < 0){
+        //     while (la <= lb){
+        //         answer += la;
+        //         la++;
+        //     }
+        // }
+        // else{
+        //     answer = la;
+        // }
         return answer;
     }
 }
