4670c6bde82d67e631fb7c7250d78a059016c46d
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-09-27 19:08:51 +0900

    [level 1] Title: 시저 암호, Time: 0.40 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.cs
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.cs
@@ -4,31 +4,42 @@ public class Solution {
     public string solution(string s, int n) {
         string answer = "";
         StringBuilder sb = new StringBuilder();
-        string low = "abcdefghijklmnopqrstuvwxyz";
-        string up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char[] lowch = low.ToCharArray();
-        char[] upch = up.ToCharArray();
         
         int index = 0; char ch =' ';
         
         for (int i = 0; i<s.Length; i++){
             ch = s[i];
-            if(ch == ' '){
+            if((ch >= 'A' && ch <='Z') || (ch>='a' && ch <= 'z')){
+                //문자                
+                if(Char.IsLower(ch)){
+                    sb.Append( (char)('a' + (ch + n -'a') % 26));
+                }else{
+                    sb.Append( (char)('A' + (ch+n -'A') % 26));
+                }
+                
+            }            
+            else{
                 // 공백
                 sb.Append(' ');
                 continue;
             }
-            else{
-                // 문자
-                char[] search = Char.IsLower(ch) ? lowch:upch;
+            
+            
+        // string low = "abcdefghijklmnopqrstuvwxyz";
+        // string up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // char[] lowch = low.ToCharArray();
+        // char[] upch = up.ToCharArray();
+//             else{
+//                 // 문자
+//                 char[] search = Char.IsLower(ch) ? lowch:upch;
                 
-                index = Array.IndexOf(search, ch);
-                if(index+n >= search.Length){
-                    sb.Append(search[index+n - search.Length]);
-                }else{
-                    sb.Append(search[index+n]);
-                }
-            }
+//                 index = Array.IndexOf(search, ch);
+//                 if(index+n >= search.Length){
+//                     sb.Append(search[index+n - search.Length]);
+//                 }else{
+//                     sb.Append(search[index+n]);
+//                 }
+//             }
         }
         
         return sb.ToString();
