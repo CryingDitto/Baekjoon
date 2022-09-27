b5bd196efa9a030c331331bc2e9b8395bc5ca54d
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-09-27 17:49:07 +0900

    [level 1] Title: 2016년, Time: 0.92 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.cs
+++ 프로그래머스/lv1/12901. 2016년/2016년.cs
@@ -3,8 +3,8 @@ public class Solution {
     public string solution(int a, int b) {
         string answer = "";
         
-        DateTime dt = DateTime.ParseExact($"2016 {a} {b}", "yyyy M d", null);
-        
+        // DateTime dt = DateTime.ParseExact($"2016 {a} {b}", "yyyy M d", null);
+        DateTime dt = new DateTime(2016, a, b);
         return dt.ToString("ddd").ToUpper();
     }
 }
