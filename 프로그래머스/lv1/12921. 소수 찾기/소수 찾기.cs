e9ffe899cc95a045ae727e92df6a238fad0927fd
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-10-05 19:43:14 +0900

    [level 1] Title: 소수 찾기, Time: 12.70 ms, Memory: 34.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] nums = new int[n+1];
+        
+        // Array 각 방에 1부터 n까지 채우기
+        // index 2인 방에는 2, n인 방에는 n 들어감
+        // 1은 소수 아니므로 제외
+        for (int i = 2; i<n+1; i++){
+            nums[i] = i;
+        }
+        // 에라토스테네스의 체
+        for (int i=2; i<=n; i++){
+            if(nums[i]==0) continue; // 이미 소수 아님이 판명된 방 연산 skip
+            
+            for (int j=i*2; j<=n; j+=i){
+                nums[j] = 0; // i의 배수 전부 소수가 아님
+            }
+        }
+        
+        // 정답 구하기
+        foreach(var num in nums){
+            if(num!=0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
