5ef20a87565edcdfab5495db8a524af15d5f389d
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-10-05 19:48:38 +0900

    [level 1] Title: 소수 찾기, Time: 11.40 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -6,9 +8,11 @@ public class Solution {
         // Array 각 방에 1부터 n까지 채우기
         // index 2인 방에는 2, n인 방에는 n 들어감
         // 1은 소수 아니므로 제외
-        for (int i = 2; i<n+1; i++){
-            nums[i] = i;
-        }
+        // for (int i = 2; i<n+1; i++){
+        //     nums[i] = i;
+        // }
+        nums = Enumerable.Range(0, n+1).ToArray();
+        nums[1]=0;
         // 에라토스테네스의 체
         for (int i=2; i<=n; i++){
             if(nums[i]==0) continue; // 이미 소수 아님이 판명된 방 연산 skip
