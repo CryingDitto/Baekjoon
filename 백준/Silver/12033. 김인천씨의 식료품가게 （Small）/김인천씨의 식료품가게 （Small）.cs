15f3d89cd6d45832f730f1e960c5a267dd887a18
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-10-20 15:42:45 +0900

    Update 김인천씨의 식료품가게 （Small）.cs
    앞에서부터 뒤로 검사

--- 백준/Silver/12033. 김인천씨의 식료품가게 （Small）/김인천씨의 식료품가게 （Small）.cs
+++ 백준/Silver/12033. 김인천씨의 식료품가게 （Small）/김인천씨의 식료품가게 （Small）.cs
@@ -8,7 +8,7 @@ namespace Baekjoon
         static void Main(string[] args)
         {
             int cases = int.Parse(Console.ReadLine());
-            
+
             for (int i = 0; i < cases; i++)
             {
                 int product = int.Parse(Console.ReadLine());
@@ -17,9 +17,9 @@ namespace Baekjoon
                 List<int> discount = new List<int>();
                 List<int> normalPrice = new List<int>();
                 bool[] visited = new bool[price.Count];
-                
+
                 int temp = 0;
-                for (int j = 0; j<price.Count; j++)
+                for (int j = 0; j < price.Count; j++)
                 {
                     if (!visited[j])
                     {
@@ -29,42 +29,26 @@ namespace Baekjoon
                         {
                             // 3의 배수이면 할인가일 가능성 존재
                             int index = price.IndexOf(temp / 3 * 4);
-                            //Console.WriteLine("Initial Index: " + index);
-                            if(index >= 0) // 문제 조건에 따르면 안 해도 되는데 그냥 안전용.
+                            if (index >= 0) // 문제 조건에 따르면 안 해도 되는데 그냥 안전용.
                             {
-                                while (visited[index])
-                                {
-                                    // 이미 사용한 숫자이므로 새로운 index 구함
-                                    // 찾은 index 다음 숫자부터 다시 temp/3*4 검색
-                                    // visited에서 false인 자리 찾으면 while 탈출
-                                    index = index + 1 + price.GetRange(index+1, price.Count - index - 1).IndexOf(temp / 3 * 4);
-                                    //Console.WriteLine("New index: " + index);
-
-                                }
                                 if (!visited[index])
                                 {
-                                    //Console.WriteLine("Normal Price Index: "+index);
-                                    //normalPrice.Add(price[index]);
                                     discount.Add(temp);
                                     price[index] = 0;
                                     visited[index] = true;
-                                }                                
-                            }                            
+                                }
+                            }
                         }
-                    }                    
+                    }
                 }
                 // print
                 Console.Write($"Case #{i + 1}:");
-                foreach(var p in discount)
+                foreach (var p in discount)
                 {
                     Console.Write($" {p}");
                 }
-                if (i < cases - 1)
-                {
-                    Console.Write("\n");
-                }
+                Console.Write("\n");                
             }
-
         }
     }
 }
