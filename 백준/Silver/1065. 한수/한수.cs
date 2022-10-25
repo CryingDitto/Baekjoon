d9609b55415a040ddda1a6f701e308f00ef15f44
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-10-25 11:38:52 +0900

    [Silver IV] Title: 한수, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.cs
+++ 백준/Silver/1065. 한수/한수.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text;
 namespace Baekjoon
 {
     class Program
@@ -8,7 +7,6 @@ namespace Baekjoon
         {
             int n = int.Parse(Console.ReadLine());
             int count = 0;
-            StringBuilder sb = new StringBuilder();
 
             if (n < 100) count = n;
             else
@@ -16,8 +14,7 @@ namespace Baekjoon
                 count = 99;
                 for (int i = 100; i <= n; i++)
                 {
-                    sb.Clear();
-                    sb.Append(i.ToString());
+                    string sb = i.ToString();
                     if (sb[0] - sb[1] == sb[1] - sb[2]) count++;
                 }
             }            
