0caadd89f829463584fa0ca3aa6fb0ed105c1fa6
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2022-12-22 09:38:40 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 68 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cs
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cs
@@ -1,58 +1,41 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-namespace Baekjoon
+int[] nm = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+string[] wbString = new string[nm[0]];
+for(int i=0; i<nm[0]; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] nm = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            StringBuilder sb = new StringBuilder();
-            List<int> countList = new List<int>();
-            int countw = 0;
-            int countb = 0;
-
-            for (int i = 0; i < nm[0]; i++)
-            {
-                sb.AppendLine(Console.ReadLine());
-            }
-            string[] inputs = sb.ToString().Split('\n');
-
-            for (int i = 0; i < nm[0]-7; i++)
-            {
-                for (int j = 0; j < nm[1] - 7; j++)
-                {
-                    countw = 0;
-                    countb = 0;
-
-                    for (int a = i; a<i+8; a++)
-                    {
-                        for (int c = j; c < j+8; c++)
-                        {
-                            if ( (a + c) % 2 == 0)
-                            {
-                                // 첫 글자가 W라고 가정한 경우 (a+c)가 짝수일 때 W여야 함
-                                if (inputs[a][c] != 'W') {countw++;}
-                                // 첫 글자가 B라고 가정한 경우 (a+c)가 짝수일 때 B여야 함
-                                if (inputs[a][c] != 'B') {countb++;}
-                            }
-                            else
-                            {
-                                // 첫 글자가 W라고 가정한 경우 (a+c)가 홀수일 때 B여야 함
-                                if (inputs[a][c] != 'B') {countw++;}
-                                // 첫 글자가 B라고 가정한 경우 (a+c)가 홀수일 때 W여야 함
-                                if (inputs[a][c] != 'W') {countb++;}
-                            }
-
-                        }
-                    }
-                    countList.Add(countw);
-                    countList.Add(countb);
-                }                
-            }
-           Console.WriteLine(countList.Min());
-        }
-    }
+    wbString[i] = Console.ReadLine();
 }
+
+List<int> sums = new List<int>();
+
+for(int i = 0; i<nm[0]-7; i++)
+{
+    for(int j=0; j<nm[1]-7; j++)
+    {
+        int wCount = 0; // W로 시작했을 때 칠해야 하는 개수
+        int bCount = 0; // B로 시작했을 때 칠해야 하는 개수
+
+        for(int row=i; row<i+8; row++)
+        {
+            for(int col=j; col<j+8; col++)
+            {
+                if((row+col)%2 == 0)
+                {
+                    if (wbString[row][col] - 'W' == 0) bCount++; // B 시작이라고 가정
+                    else if (wbString[row][col] - 'B' == 0) wCount++; // W 시작이라고 가정
+                }
+                else
+                {
+                    if (wbString[row][col] - 'B' == 0) bCount++;
+                    else if (wbString[row][col] - 'W' == 0) wCount++;
+                }
+
+            }
+        }
+        sums.Add(wCount);
+        sums.Add(bCount);
+    }
+
+}
+Console.WriteLine(sums.Min());
